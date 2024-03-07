b976a66786c75b4b4bd7bdc29e401d487cf51758
Abdullah Rehman <124627894+abdullah-mirza-baig@users.noreply.github.com>
2024-03-08 02:24:40 +0500

    Add files via upload

---
+++ chapter 4  and 5/app.js
@@ -0,0 +1,210 @@
+// chapter 4
+
+//q1
+
+// let a,b,c;
+
+
+
+// q2
+
+
+// legal
+
+// let abc;
+// let ali_ahmed;
+// let your_first_name;
+// let $abdi;
+// let _age;
+
+
+// illegal
+
+
+// let 123;
+// let #abdi;
+// let your name;
+// let 1name;
+// let your & father Name;
+
+// q3
+
+// document.write("A heading stating “Rules for naming JS variables </br> Variable names can only contain $, alphabets, _ and numbers </br> For Example : $my_1stVariable </br> Variables must begin with a alphabets or $,_ </br> Variable names are case sensitive </br> Variable names should not be JS")
+
+// chapter 5
+
+// q1
+
+// let a = 5;
+// let b = 6;
+// let sum = a + b;
+// document.write("sum of a and b is : ", sum);
+
+
+
+//q2
+
+// let a = 5;
+// let b = 6;
+// let sum = a - b;
+// document.write("substraction of a and b is : ", sum);
+
+// let a = 5;
+// let b = 6;
+// let sum = a / b;
+// document.write("division of a and b is : ", sum);
+
+// let a = 5;
+// let b = 6;
+// let sum = a * b;
+// document.write("multiplication of a and b is : ", sum);
+
+// let a = 5;
+// let b = 6;
+// let sum = a % b;
+// document.write("modulus of a and b is : ", sum);
+
+
+
+// q3
+
+
+// let a;
+
+
+// document.write("value of variable after declaration is undefined");
+
+// a= 10;
+
+// document.write("</br> Initial Value is ", a)
+
+// a++;
+
+// document.write("</br> value after increment is, ", a);
+
+// a = a+3;
+
+// document.write("</br> value after  addition is ", a);
+
+// a--;
+
+// document.write("</br> after decrement ", a);
+
+// a = a % 13;
+
+// document.write("</br> the reminder is ", a);
+
+
+// q4
+
+// let price_of_ticket = 600;
+// let total_cost = price_of_ticket*5;
+// document.write("the total cost of 5 ticket is ", total_cost);
+
+
+// q5
+
+// for (var i = 1; i <= 10; i++) {
+   
+//     document.write("</br> 4 x ",i,"=", i *4, );
+   
+// }
+
+
+// q6
+
+// let a = 25;
+// let f;
+
+// f = (a * 9/ 5 ) + 32 ;
+
+// document.write("</br> C is 25deg then F is ",f);
+
+
+// let b = 70;
+// let c;
+
+
+// c = (b - 32 ) * 5/9;
+// document.write("</br> F is 70deg then C is ",c);
+
+
+
+// let item1_p = 500;
+// let item2_p = 300;
+// let item1 = +prompt("enter how many item 1 you want ");
+// let item2 = +prompt("enter how many item 2 you want");
+// let shipping_charges = 100;
+// let total_bill = (item1_p * item1) + (item2_p * item2) + shipping_charges;
+
+
+
+// document.write("</br> amount of item 1 : ",item1_p, "</br> amount of item 2 : ",item2_p, "</br> quantity of item 1 and 2 respectively ", item1 ," , " , item2 , "</br> shipping charges are :", shipping_charges);
+
+// document.write("</br> your total bill is ",total_bill);
+
+
+// q8
+
+// let total_marks = 1200;
+// let marks_obtained = 850;
+// let percentage = (marks_obtained / total_marks) * 100;
+
+// document.write("</br> total marks ", total_marks , "</br> marks obtained : ", marks_obtained ,  "</br> percentage :  ", percentage);
+
+// q9
+
+
+// let us_dollar = 10;
+// let riyal = 25;
+
+
+// let total_in_pkr = (us_dollar * 104.80) + (riyal * 28);
+
+
+// document.write("total currency in PKR: ", total_in_pkr );
+
+
+// q10
+
+
+// let a = 5;
+
+// a = ( (a+5) * 10 ) / 2;
+
+// q11
+
+// let current_year = 2024;
+// let birth_year = +prompt("Enter Your Birth Year");
+// let age = current_year - birth_year;
+
+
+// document.write("Your Age Is " , age);
+
+
+// let r = 20;
+// const pi = 3.142;
+
+// let circumference = 2 * pi * r;
+
+// let area = pi * r**2;
+
+// document.write("the circumference of circle is : ", circumference);
+// document.write("</br> the are of circle is : ", area);
+
+
+
+// q13
+
+// let fvrt_snack = prompt("enter your fvrt snack");
+// let current_age = +prompt("enter your current age");
+// let max_age = 65;
+// let snacks_per_day = +prompt("snack per day");
+// let total = ( (max_age - current_age) * 365) * snacks_per_day;
+
+
+// document.write("YOur Favrt Snakc is : ", fvrt_snack , "</br> your current age is : ", current_age , "</br> max age is :" , max_age ,  "</br> snack per day : ", snacks_per_day , "</br> You will need ", total , " to last you until the ripe old age of " , max_age);
+
+
+
+

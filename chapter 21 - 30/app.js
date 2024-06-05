1be6ca8633229a697ce2f7b12faa49360840f5e5
Abdullah Rehman <124627894+abdullah-mirza-baig@users.noreply.github.com>
2024-06-05 19:38:46 +0500

    Add files via upload

---
+++ chapter 21 - 30/app.js
@@ -0,0 +1,56 @@
+// 
+
+// 2
+// var favoritePhoneModel = prompt("Enter your favorite mobile phone model:");
+
+// var lengthOfInput = favoritePhoneModel.length;
+
+// document.write( "My Fav phone is: " + favoritePhoneModel +"</br> The length of letter is: " + lengthOfInput);
+
+//  3
+// var word = "Pakistani";
+
+// var indexOfN = word.indexOf("n");
+
+// document.write("String: Pakistani </br> Index of 'n': " + indexOfN);
+
+
+// 4
+// var word = "Hello World";
+
+// var lastIndex = word.lastIndexOf("l");
+
+// document.write("String: Hello World </br> Last Index of 'l': " + lastIndex);
+
+// 5
+// var word = "Pakistani";
+
+// var charAtIndex3 = word.charAt(3);
+
+// document.write("Sting: Pakistani </br> Character at index is: " + charAtIndex3);
+
+
+// 6
+// var firstName = prompt("Enter your first name:");
+// var lastName = prompt("Enter your last name:");
+
+// var fullName = firstName.concat(" ", lastName);
+
+// alert("Hello, " + fullName + "! Welcome!");
+
+// // 7
+// var word = "Hyderabad";
+
+// var replacedWord = word.replace("Hyder", "Islam");
+// document.write("City: " + word);
+// document.write("</br> After replacement: " + replacedWord);
+
+// 8
+// var message = "Ali and Sami are best friends. They play cricket and football together.";
+
+// var replacedMessage = message.replace(/and/g, "&");
+
+// document.write("After replacement: " + replacedMessage);
+
+// 9
+
